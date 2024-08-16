aca06f74fc59675ca1a9e0b0cb5acdd51cb94b66
niewyspana <miss.polazava@gmail.com>
2024-08-16 21:47:46 +0200

    add gitignore and requested changes

--- Kino/AppDelegate.swift
+++ Kino/AppDelegate.swift
@@ -7,42 +7,36 @@
 
 import UIKit
 
-
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        let movies = MainViewController(nibName: "MainViewController", bundle: nil)
-        movies.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "movieclapper"), tag: 0)
-
-        let ticket = TicketsViewController(nibName: "TicketsViewController", bundle: nil)
-        ticket.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "ticket"), tag: 1)
-
-        let fav = FavouritesViewController(nibName: "FavouritesViewController", bundle: nil)
-        fav.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "heart.fill"), tag: 2)
-
+        
+        func createNavController(for rootViewController: UIViewController, title: String, imageName: String) -> UINavigationController {
+            rootViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: imageName), tag: 0)
+            rootViewController.navigationItem.title = title
+            rootViewController.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.star"), style: .plain, target: self, action: #selector(leftButtonTapped))
+            rootViewController.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "bell.badge"), style: .plain, target: self, action: #selector(rightButtonTapped))
+            return UINavigationController(rootViewController: rootViewController)
+        }
+        
+        let movies = createNavController(for: MainViewController(nibName: "MainViewController", bundle: nil), title: "KinkiNO", imageName: "movieclapper")
+        let tickets = createNavController(for: TicketsViewController(nibName: "TicketsViewController", bundle: nil), title: "Tickets", imageName: "ticket")
+        let favourites = createNavController(for: FavouritesViewController(nibName: "FavouritesViewController", bundle: nil), title: "Favourites", imageName: "heart.fill")
         
         let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [movies, ticket, fav]
-        
-        let navigationController = UINavigationController(rootViewController: tabBarController)
-
-        tabBarController.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.star"), style: .plain, target: self, action: #selector(leftButtonTapped))
-        tabBarController.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "bell.badge"), style: .plain, target: self, action: #selector(rightButtonTapped))
-        tabBarController.navigationItem.title = "KinkiNo"
+        tabBarController.viewControllers = [movies, tickets, favourites]
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
-
+        
         return true
     }
-
+    
     @objc func leftButtonTapped() { }
-
+    
     @objc func rightButtonTapped() { }
 }
