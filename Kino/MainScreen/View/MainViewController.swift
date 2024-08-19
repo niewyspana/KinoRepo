dceb33eba9ad30527ce2f1c8c84d65454172d479
niewyspana <miss.polazava@gmail.com>
2024-08-19 15:51:14 +0200

    KIN-4 Implemented UICollectonViewCell

--- Kino/MainScreen/View/MainViewController.swift
+++ Kino/MainScreen/View/MainViewController.swift
@@ -13,11 +13,15 @@ class MainViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     private var collectionHeaderView = HeaderView.loadFromNib()
+    private var tableHeaderView = HeaderView.loadFromNib()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addHeaderView()
         addTableHeaderView()
+        addTableView()
+        setUpCollectionView()
+        
     }
     
     private func addHeaderView() {
@@ -34,7 +38,7 @@ class MainViewController: UIViewController {
     }
     
     private func addTableHeaderView() {
-        let tableHeaderView = HeaderView.loadFromNib()
+        
         tableHeaderView.configure(with: "Popular", buttonTitle: "See more")
         view.addSubview(tableHeaderView)
         
@@ -47,13 +51,68 @@ class MainViewController: UIViewController {
         )
     }
     
+    private func addTableView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [tableView.topAnchor.constraint(equalTo: tableHeaderView.bottomAnchor),
+             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)]
+        )
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         refreshCollectionViewTopConstraint()
     }
     
     private func refreshCollectionViewTopConstraint() {
         collectionViewTopConstraint.constant = collectionHeaderView.frame.maxY
     }
+    
+    private func setUpCollectionView() {
+        collectionView.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 144, height: 280)
+        layout.scrollDirection = .horizontal
+        collectionView.setCollectionViewLayout(layout, animated: true)
+        
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    }
 }
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("You tapped me")
+    }
+}
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
+        
+        let mainImage = UIImage(named: "image")!
+        let starImage = UIImage(named: "star")!
+        
+        
+        let movieTitle = "The Neverending Story\(indexPath.row + 1)"
+        let rating = "9.8/10.0 IMDb"
+        
+        
+        cell.configure(with: mainImage, star: starImage, title: movieTitle, rating: rating)
+        return cell
+    }
+}
+
+
+
+
